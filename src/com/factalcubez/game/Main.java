f4febd838d9686576d0d1a3bc94e47fa19b16209
Nguyen Hoang Duc <nduc7322@gmail.com>
2023-12-14 13:16:35 +0700

    Nguyen Hoang Duc

---
+++ src/com/factalcubez/game/Main.java
@@ -0,0 +1,18 @@
+package com.factalcubez.game;
+import javax.swing.JFrame;
+
+    public class Main {
+        public void main(String[] args) {
+            Game game = new Game();
+            JFrame window = new JFrame("2048");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setResizable(false);
+            window.add(game);
+            window.pack();
+            window.setLocationRelativeTo(null);
+            window.setVisible(true);
+            game.start();
+
+
+        }
+    }
