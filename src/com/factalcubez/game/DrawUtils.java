20a2fe552c59b8e3ccdacbb4d186a02c196d5558
huy.tranminh <tmhuyy.itiu@gameloft.com>
2023-12-17 11:16:41 +0700

    - rename getMassageWidth to getMessageWidth

--- src/com/factalcubez/game/DrawUtils.java
+++ src/com/factalcubez/game/DrawUtils.java
@@ -6,7 +6,7 @@ import java.awt.font.TextLayout;
 import java.awt.geom.Rectangle2D;
 public class DrawUtils {
     private DrawUtils(){ }
-    public static int getMassageWidth(String message, Font font, Graphics2D g){
+    public static int getMessageWidth(String message, Font font, Graphics2D g){
         g.setFont(font);
         Rectangle2D bounds = g.getFontMetrics().getStringBounds(message, g);
         return(int) bounds.getWidth();
